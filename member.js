8fc92b8a26047a07000576af4f1caf2fa1771216
Zakariae Kada <beetlejuicing22@gmail.com>
2024-12-07 14:54:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    let member = {
+        name: "John Doe",
+        skills: ["Javascript", "React", "Node"],
+        details: {
+            address: "123 Main St",
+            city: "Pittsburgh",
+            state: "PA"
+        }
+    }
+    return member;
+}
